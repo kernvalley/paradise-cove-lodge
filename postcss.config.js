d3ec1c18bb5b7b43bf175e5f13b8b77bf66e7b6d
Chris Zuber <shgysk8zer0@gmail.com>
2020-07-06 16:19:29 -0700

    Enable linting of config files

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,9 @@
+/* eslint-global module, require */
 module.exports = {
 	"map": {inline: false},
 	"plugins": [
 		require("postcss-import"),
-		require("postcss-url"),
+		// require("postcss-url"),
 		require('postcss-import-url'),
 		require("postcss-preset-env"),
 		require("postcss-discard-comments"),
