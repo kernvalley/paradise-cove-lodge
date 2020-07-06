c6d4304dd8d5605acdc3de1937920e1660de665a
Chris Zuber <shgysk8zer0@gmail.com>
2020-07-06 16:13:42 -0700

    Update PostCSS plugins
    Mostly, replace postcss-cssnext with postcss-preset-env

--- postcss.config.js
+++ postcss.config.js
@@ -4,8 +4,10 @@ module.exports = {
 		require("postcss-import"),
 		require("postcss-url"),
 		require('postcss-import-url'),
-		// require("postcss-cssnext"),
+		require("postcss-preset-env"),
 		require("postcss-discard-comments"),
+		require('postcss-custom-properties'),
+		require('postcss-media-minmax'),
 		require("cssnano"),
 	]
 };
