807ca2be6348dd715492d11f08d0ca753f9eec93
Chris Zuber <shgysk8zer0@gmail.com>
2020-09-14 17:51:44 -0700

    Use CDN Analytics handlers
    No longer provide own

--- js/index.js
+++ js/index.js
@@ -10,19 +10,18 @@ import 'https://cdn.kernvalley.us/components/pwa/install.js';
 import 'https://cdn.kernvalley.us/components/weather-current.js';
 import { ready, $ } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
 import { loadScript } from 'https://cdn.kernvalley.us/js/std-js/loader.js';
-import { importGa } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
-import { updateImage, outbound, madeCall } from './functions.js';
+import { importGa, externalHandler, telHandler, mailtoHandler, geoHandler, genericHandler } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
 import { GA } from './consts.js';
+import { updateImage } from './functions.js';
 
+document.documentElement.style.setProperty('--viewport-height', window.innerHeight);
+document.documentElement.classList.replace('no-js', 'js');
+document.documentElement.classList.toggle('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
+document.documentElement.classList.toggle('no-details', document.createElement('details') instanceof HTMLUnknownElement);
 
 requestIdleCallback(() => {
 	const $doc = $(document.documentElement);
 
-	$doc.replaceClass('no-js', 'js');
-	$doc.toggleClass('no-dialog', document.createElement('dialog') instanceof HTMLUnknownElement);
-	$doc.toggleClass('no-details', document.createElement('details') instanceof HTMLUnknownElement);
-
-	$doc.css({'--viewport-height': `${window.innerHeight}px`});
 	$doc.debounce('resize', () => $doc.css({'--viewport-height': `${window.innerHeight}px`}));
 
 	if (typeof GA === 'string' && GA !== '') {
@@ -34,15 +33,19 @@ requestIdleCallback(() => {
 
 			await ready();
 
-			$('a[rel~="external"]').click(outbound, { passive: true, capture: true });
-			$('a[href^="tel:"]').click(madeCall, { passive: true, capture: true });
+			$('a[rel~="external"]').click(externalHandler, { passive: true, capture: true });
+			$('a[href^="tel:"]').click(telHandler, { passive: true, capture: true });
+			$('a[href^="mailto:"]').click(mailtoHandler, { passive: true, capture: true });
+			$('a[href^="geo:"]').click(geoHandler, { passive: true, capture: true });
+			$('[data-event-category][data-event-label]').click(genericHandler, { passive: true, capture: true });
 		}).catch(console.error);
 	}
-
 });
 
-
-Promise.allSettled([ready(), loadScript('https://cdn.polyfill.io/v3/polyfill.min.js')]).finally(() =>{
+Promise.allSettled([
+	ready(),
+	loadScript('https://cdn.polyfill.io/v3/polyfill.min.js'),
+]).then(() =>{
 	if (location.pathname.startsWith('/lakecam')) {
 		updateImage(document.getElementById('lake-cam-img'), 15);
 	} else if (location.pathname.startsWith('/menu')) {
